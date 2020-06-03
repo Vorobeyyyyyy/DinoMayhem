5b9f6fd1e81a58817177acec09b200392912137f
Vadim <pyrko.safiya@mai.ru>
2020-06-03 21:57:29 +0300

    IntTeam

--- core/src/com/sosoft/skyfighter/levels/Menu.java
+++ core/src/com/sosoft/skyfighter/levels/Menu.java
@@ -23,22 +23,16 @@ public class Menu implements Screen {
     private Stage stage;
 
     Texture tempTxt1;
-    Texture tempTxt2;
     Texture tempTxt3;
-    Texture tempTxt4;
     Texture tempTxt5;
-    Texture tempTxt6;
     Texture tempTxt7;
     Texture tempTxt8;
 
     private Image mainArt1;
-    private Image mainPanel;
     private Image nameGame;
-    private Image mainMenu;
 
     private Image settingsImage;
     private Image playImage;
-    private Image playOnlineImage;
     private Image exitImage;
 
     private Music menuMusic;
@@ -50,37 +44,20 @@ public class Menu implements Screen {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
 
-        tempTxt1 = new Texture("Menu\\MainArt5.jpg");
+        tempTxt1 = new Texture("Menu\\Desert1.jpg");
         mainArt1 = new Image(tempTxt1);
         mainArt1.setPosition(0, 0);
         mainArt1.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-        tempTxt2 = new Texture("Menu\\BlackRec.png");
-        mainPanel = new Image(tempTxt2);
-        mainPanel.setPosition(-Gdx.graphics.getWidth()/3.8f, 0);
-        mainPanel.setSize(Gdx.graphics.getWidth()/3.8f, Gdx.graphics.getHeight());
-        mainPanel.addAction(sequence(alpha(0.7f), parallel(moveBy(Gdx.graphics.getWidth()/3.8f, 0, 0.4f))));
-
-        tempTxt3 = new Texture("Menu\\NameGame.png");
+        tempTxt3 = new Texture("Menu\\DinoMayhem.png");
         nameGame = new Image(tempTxt3);
-        nameGame.setSize(Gdx.graphics.getWidth()/4.8f, nameGame.getHeight()/3.6f);
-        nameGame.setOrigin(nameGame.getWidth() * 0.7f, nameGame.getHeight());
-        nameGame.addAction(sequence(alpha(0), scaleTo(.1f, .1f),
-                parallel(fadeIn(1f, Interpolation.pow2),
-                        scaleTo(2f, 2f, 1.5f, Interpolation.swing),
-                        moveTo(stage.getWidth() * 0.65f, stage.getHeight() * 0.8f))));
-
-        tempTxt4 = new Texture("Menu\\MainMenu.png");
-        mainMenu = new Image(tempTxt4);
-        mainMenu.setSize(Gdx.graphics.getWidth()/3.8f, Gdx.graphics.getHeight()/3.9f);
-        mainMenu.setPosition(-Gdx.graphics.getWidth()/3.2f, 0.75f * Gdx.graphics.getHeight());
-        mainMenu.addAction(moveBy(Gdx.graphics.getWidth()/3.2f, 0, 0.4f));
+        nameGame.setSize(Gdx.graphics.getWidth()/2.8f, Gdx.graphics.getHeight()/5.2f);
+        nameGame.setPosition(Gdx.graphics.getWidth()/2 - nameGame.getWidth()/2, Gdx.graphics.getHeight() - nameGame.getHeight());
 
         tempTxt5 = new Texture("Menu\\PlayImage.png");
         playImage = new Image(tempTxt5);
         playImage.setSize(Gdx.graphics.getWidth()/3.8f, Gdx.graphics.getHeight()/7.2f);
-        playImage.setPosition(-Gdx.graphics.getWidth()/3.2f, 0.75f * Gdx.graphics.getHeight() );
-        playImage.addAction(moveBy(Gdx.graphics.getWidth()/3.2f, 0, 0.4f));
+        playImage.setPosition(Gdx.graphics.getWidth()/2 - playImage.getWidth()/2, Gdx.graphics.getHeight() - nameGame.getHeight() - playImage.getHeight());
         playImage.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -102,34 +79,10 @@ public class Menu implements Screen {
             }
         });
 
-        tempTxt6 = new Texture("Menu\\PlayOnlineImage.png");
-        playOnlineImage = new Image(tempTxt6);
-        playOnlineImage.setSize(Gdx.graphics.getWidth()/3.8f, Gdx.graphics.getHeight()/7.2f);
-        playOnlineImage.setPosition(-Gdx.graphics.getWidth()/3.2f, 0.75f * Gdx.graphics.getHeight() -  playImage.getHeight());
-        playOnlineImage.addAction(moveBy(Gdx.graphics.getWidth()/3.2f, 0, 0.4f));
-        playOnlineImage.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-            }
-
-            @Override
-            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
-                playOnlineImage.setColor(Color.GOLD);
-                playOnlineImage.addAction(sizeBy(10f, 10f, 0.3f));
-            }
-
-            @Override
-            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
-                playOnlineImage.setColor(Color.WHITE);
-                playOnlineImage.addAction(sizeBy(-10f, -10f, 0.3f));
-            }
-        });
-
         tempTxt7 = new Texture("Menu\\SettingsImage.png");
         settingsImage = new Image(tempTxt7);
         settingsImage.setSize(Gdx.graphics.getWidth()/3.8f, Gdx.graphics.getHeight()/7.2f);
-        settingsImage.setPosition(-Gdx.graphics.getWidth()/3.2f, 0.75f * Gdx.graphics.getHeight() - 2 * playImage.getHeight());
-        settingsImage.addAction(moveBy(Gdx.graphics.getWidth()/3.2f, 0, 0.4f));
+        settingsImage.setPosition(Gdx.graphics.getWidth()/2 - settingsImage.getWidth()/2, Gdx.graphics.getHeight() - nameGame.getHeight() - playImage.getHeight() - settingsImage.getHeight());
         settingsImage.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -151,8 +104,7 @@ public class Menu implements Screen {
         tempTxt8 = new Texture("Menu\\ExitImage.png");
         exitImage = new Image(tempTxt8);
         exitImage.setSize(Gdx.graphics.getWidth()/3.8f, Gdx.graphics.getHeight()/7.2f);
-        exitImage.setPosition(-Gdx.graphics.getWidth()/3.2f, 0);
-        exitImage.addAction(moveBy(Gdx.graphics.getWidth()/3.2f, 0, 0.4f));
+        exitImage.setPosition(Gdx.graphics.getWidth()/2 - exitImage.getWidth()/2, Gdx.graphics.getHeight() - nameGame.getHeight() - playImage.getHeight() - settingsImage.getHeight() - exitImage.getHeight());
         exitImage.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -174,11 +126,8 @@ public class Menu implements Screen {
         });
 
         stage.addActor(mainArt1);
-        stage.addActor(mainPanel);
         stage.addActor(nameGame);
-        stage.addActor(mainMenu);
         stage.addActor(playImage);
-        stage.addActor(playOnlineImage);
         stage.addActor(settingsImage);
         stage.addActor(exitImage);
     }
@@ -230,11 +179,8 @@ public class Menu implements Screen {
         stage.dispose();
         menuMusic.dispose();
         tempTxt1.dispose();
-        tempTxt2.dispose();
         tempTxt3.dispose();
-        tempTxt4.dispose();
         tempTxt5.dispose();
-        tempTxt6.dispose();
         tempTxt7.dispose();
         tempTxt8.dispose();
     }
---
+++ core/src/com/sosoft/skyfighter/weapons/SVD.java
@@ -0,0 +1,47 @@
+package com.sosoft.skyfighter.weapons;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.sosoft.skyfighter.AnimatedSprite;
+import com.sosoft.skyfighter.heroes.Hero;
+
+import static com.sosoft.skyfighter.weapons.Bullet.INFDISTANCE;
+import static com.sosoft.skyfighter.weapons.Bullet.INFTIME;
+
+public class SVD extends Weapon {
+    public SVD(Hero hero) {
+        super(hero);
+        initBullet();
+        posOffset = new Vector2(0,-5);
+        animation.addAnimation("idle", new AnimatedSprite("Weapons/SVD/idle_",1,1,1.5f, posOffset));
+        animation.addAnimation("fire", new AnimatedSprite("Weapons/SVD/fire/fire_",11,1/10f,1.5f, posOffset));
+        animation.setAnimation("idle");
+
+        fireSound = Gdx.audio.newSound(Gdx.files.internal("Weapons/SVD/Sound/fire.mp3"));
+
+        name = "SVD";
+        maxAmmo = 10;
+        shotsPerSec = 2;
+        accuracy = 1f;
+        reloadTime = 3.5f;
+    }
+
+    @Override
+    public void fire() {
+        super.fire();
+    }
+
+    @Override
+    void initBullet() {
+        super.initBullet();
+        bulletDef.damage = 322;
+        bulletDef.speed = 80f;
+        bulletDef.maxDistance = INFDISTANCE;
+        bulletDef.maxTimeAlive = INFTIME;
+        bulletDef.canBounce = false;
+        bulletDef.accyuracy = 1f;
+        bulletDef.spriteName = "Heroes/Soilder/bullet.png";
+        bulletDef.scale = 0.4f;
+        bulletDef.density = 9f;
+    }
+}
